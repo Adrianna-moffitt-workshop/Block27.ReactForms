466069ce0631e51aaf9fe6fa5ba761d8239a28a7
Adrianna <adriannamoffitt06@gmail.com>
2023-11-21 15:59:50 -0600

    your message

---
+++ src/components/Authenitcate.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+export default function Authenticate({ token }) {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+  const [userName, setUserName] = useState("");
+  async function handleClick() {
+    console.log(token);
+    if (token === null) {
+      setError(
+        "Please enter username and password. Then authenticate your account."
+      );
+    } else {
+      try {
+        let response = await fetch(
+          "https://fsa-jwt-practice.herokuapp.com/authenticate",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        let json = await response.json();
+        setSuccessMessage(json.message);
+        setUserName(json.data.username.userName);
+        setError(null);
+        console.log(token);
+
+        // setUserName(json.)
+      } catch (error) {
+        console.log(token);
+        setError(error.message);
+      }
+    }
+  }
+
+  return (
+    <>
+      {/* <h2>Authenticate</h2> */}
+      {error && <p>{error}</p>}
+      {successMessage && (
+        <>
+          <h3>{successMessage}</h3> <p>Username: {userName}</p>
+        </>
+      )}
+      <button onClick={handleClick}>Authenticate Token</button>
+    </>
+  );
+}
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+
+export default function SignUpForm({ setToken }) {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log(hasNumbers(password));
+    if (userName.length < 6) {
+      setError("Username must be at least 6 characters");
+    } else if (password.length < 8 || !hasNumbers(password)) {
+      setError(
+        "Password must have at least 8 characters and contain at least 1 number"
+      );
+    } else {
+      setError("Success!");
+      try {
+        let response = await fetch(
+          "https://fsa-jwt-practice.herokuapp.com/signup",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: { userName },
+              password: { password },
+            }),
+          }
+        );
+        let result = await response.json();
+        console.log(result);
+        setToken(result.token);
+      } catch (err) {
+        setError(err.message);
+        console.log(err);
+      }
+    }
+  }
+
+  const hasNumbers = (password) => {
+    // Checks if password contains numbers
+    return /\d/.test(password);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Sign Up!</h2>
+        {error && <p>{error}</p>}
+        <label>
+          Username:{" "}
+          <input
+            value={userName}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
